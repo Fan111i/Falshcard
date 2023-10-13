be6011f102a799c053fb7aae11f1a9be5f43412b
41350 <yify@student.unimelb.edu.au>
2023-10-13 13:54:28 +1100

    Changed update file from local CSV into user's CSV files but with some bug

--- Falshcard/app.py
+++ Falshcard/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import csv
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
 
-def load_flashcards(filename='flashcards.csv'):
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def load_flashcards(filename):
     cards = []
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
@@ -17,13 +27,8 @@ def load_flashcards(filename='flashcards.csv'):
             })
     return cards
 
-def save_flashcard(question, answer, choices, filename='flashcards.csv'):
-    with open(filename, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([question, answer, ";".join(choices)])
 @app.route('/')
 def homepage():
-
     return render_template('homepage.html')
 
 @app.route('/create')
@@ -35,18 +40,44 @@ def create_flashcard():
     question = request.form['question']
     answer = request.form['answer']
     choices = request.form.getlist('choices')
-    save_flashcard(question, answer, choices)
+    flashcards = session.get('flashcards', [])
+    flashcards.append({'id': len(flashcards), 'question': question, 'answer': answer, 'choices': choices})
+    session['flashcards'] = flashcards
     return redirect(url_for('select'))
-
 
 @app.route('/select')
 def select():
-    flashcards = load_flashcards()
+    flashcards = session.get('flashcards', [])
     return render_template('select.html', flashcards=flashcards)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'csvfile' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['csvfile']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        flashcards = load_flashcards(filepath)
+        # Sort the flashcards by question
+        flashcards.sort(key=lambda x: x['question'])
+        session['question_base'] = flashcards  # Save the uploaded flashcards directly to the base
+        session['current_index'] = 0
+        session['correct_answers'] = 0
+        os.remove(filepath) 
+        return redirect(url_for('quiz'))
+
+    flash('Invalid file type')
+    return redirect(request.url)
 
 @app.route('/add_to_base', methods=['POST'])
 def add_to_base():
-    flashcards = load_flashcards()
+    flashcards = session.get('flashcards', [])
     selected_ids = request.form.getlist('selected')
     session['question_base'] = [flashcards[int(i)] for i in selected_ids]
     session['current_index'] = 0
@@ -65,11 +96,14 @@ def quiz():
         session['current_index'] += 1
         return redirect(url_for('quiz'))
 
-    return render_template('quiz.html', 
-                           flashcard=current_flashcard, 
-                           progress=session['current_index'], 
+    return render_template('quiz.html',
+                           flashcard=current_flashcard,
+                           progress=session['current_index'],
                            total=len(session['question_base']),
                            correct=session['correct_answers'])
 
 if __name__ == '__main__':
+    # Ensure the UPLOAD_FOLDER exists
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
     app.run(debug=True)
